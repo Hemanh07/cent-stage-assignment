318110d6ab291b15b4f77f4636b665965468b52c
Hemanh07 <raghuhemanth008@gmail.com>
2025-04-17 18:59:35 +0530

    mongdb connected

--- backend/server.js
+++ backend/server.js
@@ -6,28 +6,20 @@ const goalRoutes = require("./routes/goals");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const dotenv = require("dotenv");
 
-// Middleware
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
 mongoose
-  .connect(
-    "mongodb+srv://crazydeveloperr007:kitk@t112@cluster0.uwqknwm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// Routes
 app.use("/api/events", eventRoutes);
 app.use("/api/goals", goalRoutes);
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
